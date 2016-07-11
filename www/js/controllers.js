189c8d99ff7aea528d000b204887d9087b4d189d
Aquibur Rahman Khan <ark.aquib17@gmail.com>
2016-07-11 13:13:22 +0600

    WIP: shareToFacebook Function Setup

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,14 @@
 angular.module('starter.controllers', [])
 
-.controller('facebookPostSharing', function($scope, $ionicModal, $timeout) {
-$scope.TestValue = 'World';
+.controller('facebookPostSharing', function($scope) {
+  $scope.image = 'http://1.bp.blogspot.com/-whONj2eVaPM/TkLEIoj1hxI/AAAAAAAAEQo/ZyE2gApEe3k/s1600/gmc.png';
+  $scope.postName = 'This is an example post name';
+  $scope.caption = 'This is an example caption';
+  $scope.description = 'This is an example description';
+  $scope.shareToFacebook = function(image,postName, caption, description) {
+    console.log('Button');
+
+    var url = "https://www.facebook.com/dialog/feed?app_id=1617424538586903&name="+postName+"&display=popup&caption="+caption+"&description="+description+"&link="+image+"&redirect_uri=http://facebook.com&picture="+image;
+    window.open(url, '_blank');
+  }
 })
