3b4586455edb30271f6d37b842e75f6fad625b14
Aquibur Rahman Khan <ark.aquib17@gmail.com>
2016-07-11 12:43:34 +0600

    Test: Test Code Setup Done

---
+++ tests/tests.js
@@ -0,0 +1,28 @@
+describe('facebookPostSharing', function() {
+  beforeEach(module('starter'));
+  var controller,
+    scope;
+
+  beforeEach(inject(function($controller, $rootScope) {
+    scope = $rootScope.$new();
+    controller = $controller('facebookPostSharing', {
+      $scope: scope
+    });
+
+  }))
+
+  it('Should open facebook feed dialog', function() {
+
+    spyOn(window, "open").and.callFake(function() {
+     return true;
+    });
+
+  var image = 'image.jpg';
+  var postName = 'postTest';
+  var caption = 'testCaption';
+  var description = 'this is a test';
+
+    scope.shareToFacebook(image,postName, caption, description);
+    expect(window.open ).toHaveBeenCalledWith('https://www.facebook.com/dialog/feed?app_id=171126103041654&name=postTest&display=popup&caption=testCaption&description=this is a test&link=image.jpg&redirect_uri=http://facebook.com&picture=image.jpg', '_blank');
+  })
+})
