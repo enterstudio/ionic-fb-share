35281d5feb545fdbc4496d296e823c286982ed5c
mahfuz_aftab <mahfuz.cracker@gmail.com>
2016-07-12 13:07:26 +0600

    Test: Test Code for shareToFacebookUsingDirectPost

--- tests/tests.js
+++ tests/tests.js
@@ -24,6 +24,44 @@ describe('facebookPostSharing', function() {
 
     scope.shareToFacebookUsingFeedDialog(image,postName, caption, description);
     expect(window.open ).toHaveBeenCalledWith('https://www.facebook.com/dialog/feed?app_id=1617424538586903&name=postTest&display=popup&caption=testCaption&description=this is a test&link=image.jpg&redirect_uri=http://facebook.com&picture=image.jpg', '_blank');
-  //  https://www.facebook.com/dialog/feed?app_id=171126103041654&name=postTest&display=popup&caption=testCaption&description=this is a test&link=image.jpg&redirect_uri=http://facebook.com&picture=image.jpg', '_blank' ]
   })
+})
+
+describe('facebookDirectSharingCtrl', function() {
+  beforeEach(module('starter'));
+  var controller,
+    scope;
+
+  beforeEach(inject(function($controller, $rootScope, _$q_) {
+    scope = $rootScope.$new();
+    $q = _$q_;
+    controller = $controller('facebookDirectSharingCtrl', {
+      $scope: scope
+    });
+
+  }))
+
+  it('It should post to facebook successfully', function() {
+
+
+    var deferred = $q.defer();
+    deferred.resolve('resolveData');
+    spyOn(scope, 'shareToFacebookUsingNgOpenFB').and.returnValue(deferred.promise);
+
+    scope.shareToFacebookUsingDirectPost(image,postName, caption, description);
+    $rootScope.$apply();
+    expect(scope.success).toEqual('success');
+  });
+
+  it('It should failed to post on facebook', function() {
+
+
+    var deferred = $q.defer();
+    deferred.reject('rejectData');
+    spyOn(scope, 'shareToFacebookUsingNgOpenFB').and.returnValue(deferred.promise);
+
+    scope.shareToFacebookUsingDirectPost(image,postName, caption, description);
+    $rootScope.$apply();
+    expect(scope.error).toEqual('error');
+  });
 })
